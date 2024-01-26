6471312c1cbf1fbcf45fc2e810525a4b1bfcb2fa
GorraCaps <simeneblien@icloud.com>
2024-01-26 17:59:33 +0100

    litt av hvert

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import { CountUp } from 'https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js' /*Funker ikke på nettsiden */
-
-function countStart(){
-  const $counters = document.querySelectorAll(".js-count-up"),
-        options = {
-          useEasing: true,
-          useGrouping: true,
-          separator: ",",
-          decimal: "."
-        };
-
-  $counters.forEach( (item) => {
-    const value = item.dataset.value;
-    const counter = new CountUp(item, value, options);
-    counter.start();
-  });
-}
-
-new Waypoint({
-  element: document.querySelector('.level'),
-  handler: function() {
-    countStart()
-    //this.destroy() //for once
-  },
-  offset: '50%'
+window.addEventListener('click', function(e){   
+  if (document.getElementById('play').contains(e.target)){
+    var snd = new Audio("./Lyd/lamborghini_Huracan.mp3");
+    snd.play()
+  }
 });
